f61d6b7af19a98ac6b32280bc52b33a99b7a1d64
rahul9179 <rahul9179@gmail.com>
2015-10-14 12:33:22 +0530

    update session id

--- src/test/java/SeleniumIntegration/SauceTest/POMSignup.java
+++ src/test/java/SeleniumIntegration/SauceTest/POMSignup.java
@@ -20,22 +20,23 @@ public class POMSignup{
 	private static WebDriver driver = null;
 	private static String sessionId=null;
 	
-	public static void main(String[] args) throws MalformedURLException  {
-		
+	public static void main(String[] args) throws MalformedURLException{
+		/*
 		DesiredCapabilities caps = DesiredCapabilities.firefox();
         caps.setCapability("platform", "Linux");
         caps.setCapability("version", "41");
         caps.setCapability("name", "Web Driver demo Test");
         caps.setCapability("tags", "Tag1");
         caps.setCapability("build", "v1.0");
-        WebDriver driver = new RemoteWebDriver(
+        driver = new RemoteWebDriver(
                 new URL("http://amit1881:c33b1e5d-0656-41e9-87f0-5c16dc26e576@ondemand.saucelabs.com:80/wd/hub"),
                 caps);
-       
-        sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
-        System.out.println("Session Id="+ sessionId);
-	     
-	      driver = new FirefoxDriver();
+       */
+        //sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
+        //System.out.println("Session Id="+ sessionId);
+	     POMSignup ob=new POMSignup();
+	     ob.printSessionId();
+	     driver = new FirefoxDriver();
 	      
 	      
 	      
@@ -78,6 +79,22 @@ public class POMSignup{
 	      
 	      
 	}
+	
+	private void printSessionId() throws MalformedURLException {
+        DesiredCapabilities caps = DesiredCapabilities.firefox();
+        caps.setCapability("platform", "Linux");
+        caps.setCapability("version", "41");
+        caps.setCapability("name", "Web Driver demo Test");
+        caps.setCapability("tags", "Tag1");
+        caps.setCapability("build", "v1.0");
+        driver = new RemoteWebDriver(
+                new URL("http://amit1881:c33b1e5d-0656-41e9-87f0-5c16dc26e576@ondemand.saucelabs.com:80/wd/hub"),
+                caps);
+        sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
+	    String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s", sessionId, "POMSignup");
+	    System.out.println(message);
+	}
+	
 	
 }
 
